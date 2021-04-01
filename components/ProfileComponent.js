8ddedc5f66cf3cba0d7c9c1788c422d7098c8a3f
Alexander Schilling Miranda <alexander.schilling.miranda@gmail.com>
2021-04-01 18:59:23 -0300

    feat: data request with axios

--- components/ProfileComponent.js
+++ components/ProfileComponent.js
@@ -1,7 +1,57 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
+import axios from "axios";
 
 class ProfileComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userRequestConfig: {
+        method: "get",
+        url: "https://0q27loouph.execute-api.us-east-1.amazonaws.com",
+      },
+      userResult: this.getDefaultUserResult(),
+      isRequestFulfilled: false,
+      status: 404,
+    };
+  }
+
+  getDefaultUserResult() {
+    return {
+      name: "Loading...",
+      email: "Loading...",
+      phone: "Loading...",
+      nutritionist: "Loading...",
+      image: require("../assets/img/profile-default.jpg"),
+    };
+  }
+
+  updateStateFromRequest(resp) {
+    if (resp.status == 200) {
+      this.setState({
+        userResult: {
+          name: resp.data.name,
+          email: resp.data.email,
+          phone: resp.data.phone,
+          nutritionist: resp.data.nutritionist,
+          image: resp.data.image,
+        },
+      });
+    }
+
+    this.setState({
+      isRequestFulfilled: true,
+      status: resp.status,
+    });
+  }
+
+  requestUserData() {
+    axios(this.state.userRequestConfig).then((resp) => {
+      this.updateStateFromRequest(resp);
+    });
+  }
+
   render() {
     return <View></View>;
   }
